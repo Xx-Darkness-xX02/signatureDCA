7196102cd15c6f728021ed0dbef2eabfd0accb24
k.roelofs <kjell.roelofs@student.rocvf.nl>
2019-10-08 16:35:02 +0200

    x

--- maken.php
+++ maken.php
@@ -21,6 +21,10 @@
         margin-left: 10px;
     }
 
+    *{
+        box-sizing: border-box;
+    }
+
 </style>
 <script>
     function copyFooter(containerid) {
@@ -73,21 +77,21 @@ while($row = mysqli_fetch_array($data))
         echo '<span style="font-weight: bold;">' . $row['naam'] . '</span><br />';
         echo '<div style="color: '.$brandresolve[$row["logo"]]["color_primary"].'; font-style: italic;  font-size: 14px;">' . $row['functie'] . '</div><br />';
 
-        if($row['logo'] == 1) echo '<div class="lijntje">';
+        if($row['logo'] == 1) echo '<div style="display: inline-block; border-left: solid 2px #aeaeae">';
         else if($row['logo'] > 1) echo '<hr style="width: 30px; margin-left: 0;">';
 
-        echo '<i class="far fa-envelope"></i><p  class="icontekst">' . $row['email'] . '</p><br/>';
-        echo '<i class="fas fa-phone"></i><p  class="icontekst">' . $row['telefoonnummer'] . '</p><br/>';
-        echo '<i class="fab fa-wordpress-simple"></i><p class="icontekst">' . $row['website'] . '</p><br/>';
+        echo '<i class="far fa-envelope"></i><p  class="icontekst" style="display: inline-block; margin: 6px 0 0 10px;">' . $row['email'] . '</p><br/>';
+        echo '<i class="fas fa-phone"></i><p  class="icontekst" style="display: inline-block; margin: 6px 0 0 10px;">' . $row['telefoonnummer'] . '</p><br/>';
+        echo '<i class="fab fa-wordpress-simple"></i><p class="icontekst" style="display: inline-block; margin: 6px 0 0 10px;">' . $row['website'] . '</p><br/>';
 
         // twitter ROW deze is niet verplicht
         if (!empty($row['twitter'])) {
-            echo '<i class="fab fa-twitter"></i><p class="icontekst">' . $row['twitter'] . '</p><br/>';
+            echo '<i class="fab fa-twitter"></i><p class="icontekst" style="display: inline-block; margin: 6px 0 0 10px;">' . $row['twitter'] . '</p><br/>';
         }
 
         // linkedIN ROW deze is niet verplicht
         if (!empty($row['linkedin'])) {
-            echo '<i class="fab fa-linkedin-in"></i><p class="icontekst">' . $row['linkedin'] . '</p><br/>';
+            echo '<i class="fab fa-linkedin-in"></i><p class="icontekst" style="display: inline-block; margin: 6px 0 0 10px;">' . $row['linkedin'] . '</p><br/>';
         }
 
         if($row['logo'] == 1) echo '</div>';
@@ -95,8 +99,7 @@ while($row = mysqli_fetch_array($data))
 
 // dit is de functie die bepaald welke image wordt afgebeeld
 
-     echo '<img class="logoformaat" src="'.$brandresolve[$row["logo"]]["logo"].'" /><br/>';
-       
+     echo '<img src="'.$brandresolve[$row["logo"]]["logo"].'" /><br/>';
 
         //dit is de functie die bepaald of er wel of geen social media iconen moeten komen te staan
         if ($row['socialmedia'] == 'yes'){
@@ -104,14 +107,14 @@ while($row = mysqli_fetch_array($data))
             echo '<i class="fab fa-facebook-f"></i>';
             echo '<i class="fab fa-twitter"></i>';
             echo '<i class="fab fa-youtube"></i>';
-            echo '</div><hr class="streepje2 " />';
+            echo '</div><hr class="streepje2" style="width: 80%; color: #e3e3e3; " />';
         }
 
         if (!empty($row['element1']) && $row['socialmedia']== 'yes'){
-            echo '<p class="disclaimer" style="color: '.$brandresolve[$row['logo']]['color_disc'].'; font-style: italic; padding-left: 30px;  padding-right: 30px; font-family: arial;">' . $row['element1'] . '</p>';
+            echo '<p style="color: '.$brandresolve[$row['logo']]['color_disc'].'; font-style: italic; padding-left: 30px;  padding-right: 30px; font-family: arial;">' . $row['element1'] . '</p>';
         }
         elseif (!empty($row['element1']) && ($row['socialmedia']== 'no')){
-            echo '<hr class="streepje2" />';
+            echo '<hr class="streepje2" style="width: 80%; color: #e3e3e3;" />';
             echo '<p class="disclaimer" style="color: '.$brandresolve[$row['logo']]['color_disc'].'; font-style: italic; padding: 30px; ">' . $row['element1'] . '</p>';
         }
 
